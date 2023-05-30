d66ad43fb8f706e1a4f220501e32900dd6e408b1
Sontos Sharma <sontossharma926@gmail.com>
2023-05-30 17:37:07 +0600

    sailor

---
+++ assets/js/main.js
@@ -0,0 +1,204 @@
+function Header(){
+  $(".mobile-search-btn").click(function(){
+    $(".mobile-search").toggle();
+    $(this).find('i').toggleClass("fa-xmark fa-magnifying-glass");
+  })
+  $(".navbar-toggler").click(function(){
+    $(".final-nav .offcanvas").addClass("show");
+    $(".offcanvas-backdrop").addClass("show");
+  });
+  $(".offcanvas-backdrop").click(function(){
+    $(".final-nav .offcanvas").removeClass("show");
+    $(this).removeClass("show");
+  });
+}
+$(document).scroll(function(){
+  var scroll_top = $(this).scrollTop();
+  if(scroll_top > 20){
+    $("#sticky-top").addClass("fix-middleNav");
+  }else{
+    $("#sticky-top").removeClass("fix-middleNav");
+  }
+})
+function AllSliders(){
+  $(".banner-slider").slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+  $(".quick-view-slider").slick({
+    dots: false,
+    arrows: true,
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+  $(".small-slider-nav").slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.lg-slider-product',
+    // centerMode: true,
+    focusOnSelect: true
+  });
+  $(".lg-slider-product").slick({
+    dots: false,
+    arrows: true,
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.small-slider-nav'
+  });
+  // Refresh Slick Slider when modal is shown
+  $('#productQuickView').on('shown.bs.modal', function () {
+    $('.quick-view-slider').slick('refresh');
+  });
+  $(".trending-slider-wrapper").slick({
+    dots: false,
+    arrows: true,
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    margin: "10px",
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $(".product_slider").slick({
+    dots: false,
+    arrows: true,
+    infinite: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+}
+function tabFilter(){
+  $("#tabFilter li").find("button").click(function() {
+    var tabValue = $(this).attr("data-bs-target");
+
+    // Remove active class from all buttons
+    $("#tabFilter li button").removeClass("active");
+    // Add active class to the clicked button
+    $(this).addClass("active");
+
+    // Remove active class from all tab content elements
+    $("#tabFilterContent .tab-pane").removeClass("active");
+    // Add active class to the tab content element corresponding to the clicked button
+    $("#tabFilterContent #" + tabValue).addClass("active");
+    $("#" + tabValue + " .product_slider").slick("unslick").slick("refresh");
+  });
+}
+
+$(document).ready(function(){
+  Header();
+  AllSliders();
+  tabFilter();
+  shopFilters();
+
+
+  
+});
+function Modal(){
+  // Get all modal buttons
+  var modalButtons = document.querySelectorAll('.customModalBtn');
+
+  // Add click event listener to each modal button
+  modalButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      var modalId = this.getAttribute('data-modal-id');
+      var modal = document.getElementById(modalId);
+
+      // Show the modal
+      modal.classList.add('show');
+      modal.style.display = 'block';
+      modal.setAttribute('aria-hidden', 'false');
+      modal.setAttribute('aria-modal', 'true');
+    });
+  });
+
+  // Add click event listener to each modal close button
+  var modalCloseButtons = document.querySelectorAll('.modal .btn-close');
+  modalCloseButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      var modal = this.closest('.modal');
+
+      // Hide the modal
+      modal.classList.remove('show');
+      modal.style.display = 'none';
+      modal.setAttribute('aria-hidden', 'true');
+      modal.setAttribute('aria-modal', 'false');
+    });
+  });
+}
+Modal();
